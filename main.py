9cfdfa17e22d99df2caf831c50f2d3d2e10a102a
Rob Cranfill <robcranfill@gmail.com>
2023-10-16 16:29:38 -0700

    Removed dead-end tests

--- main.py
+++ main.py
@@ -17,59 +17,6 @@ import random
 OUTPUT_PIN = board.TX 
 
 
-def play_wav_pwm(filename, rate):
-    wave_file = open(filename, "rb")
-    wave = audiocore.WaveFile(wave_file)
-    audio = audiopwmio.PWMAudioOut(OUTPUT_PIN)
-    print("audio is playing:", audio.playing)
-    audio.play(wave)
-    time.sleep(4)
-
-def play_laser_pwm():
-    wave_file = open("laser20.wav", "rb")
-    wave = WaveFile(wave_file)
-    audio = AudioOut(OUTPUT_PIN)
-    while True:
-        print("audio is playing:", audio.playing)
-        if not audio.playing:
-            audio.play(wave)
-            print(f"Playing at {wave.sample_rate * 0.90} %...")
-            wave.sample_rate = int(wave.sample_rate * 0.90) # play 10% slower each time
-        time.sleep(0.1)
-
-def play_clicks():
-
-    # num_samples = 1000
-    # mean = 0
-    # std = 1
-    # samples = numpy.random.normal(mean, std, size=num_samples)
-
-    length = 8000 // 440
-    random_wave_data = array.array("H", [0] * length)
-    for i in range(length):
-        random_wave_data[i] = int(random.uniform(0, 2**15) + 2 ** 15)
-
-    dac = audiopwmio.PWMAudioOut(OUTPUT_PIN)
-    random_wave = audiocore.RawSample(random_wave_data, sample_rate=8000)
-    dac.play(random_wave, loop=True)
-    time.sleep(1)
-    dac.stop()
-
-
-# Generate one period of sine wav.
-def play_sine():
-    length = 8000 // 440
-    sine_wave = array.array("H", [0] * length)
-    for i in range(length):
-        sine_wave[i] = int(math.sin(math.pi * 2 * i / length) * (2 ** 15) + 2 ** 15)
-
-    dac = audiopwmio.PWMAudioOut(OUTPUT_PIN)
-    sine_wave_sample = audiocore.RawSample(sine_wave, sample_rate=8000)
-    dac.play(sine_wave_sample, loop=True)
-    time.sleep(1)
-    dac.stop()
-
-
 # works for one file!
 def play_with_mixer(filename):
 
@@ -91,9 +38,3 @@ def play_with_mixer(filename):
 
 play_with_mixer("audio/geiger1b.wav")
 
-# play_wav_pwm("laser2.wav", 0)
-# play_wav_pwm("geiger3.wav", 0)
-
-# play_laser_pwm()
-# play_clicks()
-# play_sine()
