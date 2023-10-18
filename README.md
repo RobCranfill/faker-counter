f3c10a975b4c39672a0cc142acf31102698ccfee
Rob Cranfill <robcranfill@gmail.com>
2023-10-17 18:14:59 -0700

    Learned some things

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ Can I get this done by Halloweeen?
 ## Things to do
 
 ### Functional Hardware
-* Feather RP2040 (so I can use a LiPo battery)
+* CircuitPython microcontroller (needs to support I2S - 3 GPIO pins)
+  * was <strike>Feather RP2040 (so I can use a LiPo battery)</strike>
+* USB battery pack
 * Actuator to increase click rate
   * Button? Easiest. Ramp up/down?
   * Slider? Faster response, but harder to do
+* Analog meter? yes!
+* LED(s)?
 
 ## Cosmetic Hardware
 * A box
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # fc 1
 
+# std python libs
 import array
 import board
 import math
+import random
 import time
+
+# adafruit libs
 import audiobusio
 import audiocore
 import audiomixer
@@ -11,8 +15,8 @@ import audiopwmio
 import digitalio
 
 
-# must be a PWM-capable pin
-OUTPUT_PIN = board.TX
+import supervisor
+supervisor.runtime.autoreload = False  # CirPy 8 and above
 
 
 # works for one file!
@@ -22,7 +26,7 @@ def play_with_mixer(audio, filename):
                             sample_rate=44100, bits_per_sample=16, samples_signed=True)
     audio.play(mixer)
 
-    print(f"Playing {filename}...")
+    # print(f"Playing {filename}...")
     wav = audiocore.WaveFile(open(filename, "rb"))
     mixer.voice[0].play(wav, loop=False)
     s = 0
@@ -34,53 +38,63 @@ def play_with_mixer(audio, filename):
 
 
 def getPWMAudio():
-    audio = audiopwmio.PWMAudioOut(OUTPUT_PIN)
-    return audio
+    # must be a PWM-capable pin - not all are
+    return audiopwmio.PWMAudioOut(board.TX)
 
 def getI2CAudio():
-    audio = audiobusio.I2SOut(board.D9, board.D10, board.D11)
+    return audiobusio.I2SOut(board.D9, board.D10, board.D11)
 
-    return audio
-
-# this works ok
+# this works ok as a test
 def test_cycle_samples(a):
     while True:
-        print("Soft....")
+        print("Low....")
         play_with_mixer(a, "audio/g1a.wav")
         play_with_mixer(a, "audio/g1b.wav")
         play_with_mixer(a, "audio/g1c.wav")
-        print("Loud....")
+        print("High....")
         play_with_mixer(a, "audio/g2a.wav")
         play_with_mixer(a, "audio/g2a.wav")
 
 
-audio = getI2CAudio()
-
-# works!
-# test_cycle_samples(audio)
+# can't go fast enough to sound good
+def test_buncha_clicks(a):
+    while True:
+        play_with_mixer(a, "audio/1click.wav")
+        r = random.randrange(0, 100) / 1000
+        # print(r)
+        time.sleep(r)
 
 
 def test_play_by_button(a):
 
-    quiet_wavs = ["audio/g1a.wav", "audio/g1b.wav", "audio/g1c.wav"]
-    loud_wavs  = ["audio/g2a.wav", "audio/g2b.wav", "audio/g2c.wav"]
+    lo_wavs = ["audio/g1a.wav", "audio/g1b.wav", "audio/g1c.wav"]
+    hi_wavs = ["audio/g2a.wav", "audio/g2b.wav", "audio/g2c.wav"]
 
     switch = digitalio.DigitalInOut(board.D5)
     switch.direction = digitalio.Direction.INPUT
     switch.pull = digitalio.Pull.UP
 
-    i_quiet = 0
-    i_loud  = 0
+    i_lo = 0
+    i_hi  = 0
     while True:
         if switch.value: # not pressed
-            print("NOT pressed")
-            play_with_mixer(a, quiet_wavs[i_quiet])
-            i_quiet = (i_quiet+1) % len(quiet_wavs)
+            play_with_mixer(a, lo_wavs[i_lo])
+            i_lo = (i_lo+1) % len(lo_wavs)
         else:
-            print("pressed")
-            play_with_mixer(a, loud_wavs[i_loud])
-            i_loud = (i_loud+1) % len(loud_wavs)
+            play_with_mixer(a, hi_wavs[i_hi])
+            i_hi = (i_hi+1) % len(hi_wavs)
 
+
+# Pick an audio interface
+# audio = getPWMAudio()
+audio = getI2CAudio()
+
+# works!
+# test_cycle_samples(audio)
+
+# test_buncha_clicks(audio)
+
+# pretty much it!
 test_play_by_button(audio)
 
 
