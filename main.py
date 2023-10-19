7fe27ffa9dde0b3dc8a8f52271d771d54b2679d1
Rob Cranfill <robcranfill@robcranfill.net>
2023-10-19 11:45:03 -0700

    Merge some code

--- main.py
+++ main.py
@@ -13,19 +13,16 @@ import audiocore
 import audiomixer
 import digitalio
 
-
+# turn off auto-reload - that messes with the audio
 import supervisor
 supervisor.runtime.autoreload = False  # CirPy 8 and above
 
 
-# Return an audio object for the I2S interface
-def get_I2C_audio():
-    return audiobusio.I2SOut(bit_clock=board.D9, word_select=board.D10, data=board.D11)
-
-def create_mixer(audio_):
+def create_mixer():
+    audio = audiobusio.I2SOut(bit_clock=board.D9, word_select=board.D10, data=board.D11)
     mixer = audiomixer.Mixer(voice_count=1, channel_count=1,
                              sample_rate=44100, bits_per_sample=16, samples_signed=True)
-    audio_.play(mixer)
+    audio.play(mixer)
     return mixer
 
 # load all the indicated wav files and return an array with the data
@@ -45,7 +42,6 @@ def play_loaded_wav(mixer_, wav_):
     mixer_.voice[0].play(wav_, loop=False)
     while mixer_.voice[0].playing:
         time.sleep(.1)
-
 
 
 # Our main loop.
@@ -107,10 +103,7 @@ def play_continuosly(mixer_, lo_wav, hi_wav):
 # Play the files one after the other, switching as needed
 def method_1():
 
-    # Hook up to the audio interface
-    audio = get_I2C_audio()
-
-    mixer = create_mixer(audio)
+    mixer = create_mixer()
 
     lo_wavs = load_wavs(["audio/g1a.wav", "audio/g1b.wav", "audio/g1c.wav"])
     hi_wavs = load_wavs(["audio/g2a.wav", "audio/g2b.wav", "audio/g2c.wav"])
@@ -120,8 +113,8 @@ def method_1():
 
 # play the low or high file continuously until we need to switch
 def method_2():
-    audio = get_I2C_audio()
-    mixer = create_mixer(audio)
+
+    mixer = create_mixer()
 
     lo_wav = load_wavs(["audio/long_lo.wav"])[0]
     hi_wav = load_wavs(["audio/long_hi.wav"])[0]
