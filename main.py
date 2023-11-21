f573695fea4c1caaa8646bb0086c70a03ac238d0
Rob Cranfill <robcranfill@gmail.com>
2023-11-20 20:19:03 -0800

    Now for Pico. (Make work for both?)

--- main.py
+++ main.py
@@ -12,27 +12,29 @@ import audiobusio
 import audiocore
 import audiomixer
 import digitalio
-import neopixel
+# import neopixel
 
 
 # turn off auto-reload - that messes with the audio
 import supervisor
 supervisor.runtime.autoreload = False  # CirPy 8 and above
 
-# defines, so to speak
-GPIO_PIN_PUSHBUTTON = board. D5 # was D6, but I moved it farther from I2S line
-PIN_GPIO_RED_LED = board.TX
-PIN_GPIO_GREEN_LED = board.D4
+# Pico version!
 
-PIN_I2S_BCLK   = board. D9
-PIN_I2S_LRC    = board.D10
-PIN_I2S_DATA   = board.D11
+# defines, so to speak
+GPIO_PIN_PUSHBUTTON = board.GP13
+PIN_GPIO_RED_LED    = board.GP14
+PIN_GPIO_GREEN_LED  = board.GP15
+
+PIN_I2S_BCLK = board.GP16
+PIN_I2S_LRC  = board.GP17
+PIN_I2S_DATA = board.GP18
 
 
 def create_mixer():
     audio = audiobusio.I2SOut(bit_clock=PIN_I2S_BCLK, word_select=PIN_I2S_LRC, data=PIN_I2S_DATA)
     mixer = audiomixer.Mixer(voice_count=1, channel_count=1,
-                             sample_rate=44100, bits_per_sample=16, samples_signed=True)
+                             sample_rate=11025, bits_per_sample=16, samples_signed=True)
     audio.play(mixer)
     return mixer
 
@@ -57,7 +59,7 @@ def play_loaded_wav(mixer_, wav_):
 
 
 # play one sound continuously until we need to switch.
-def play_continuosly(switch_, neopixel_, red_, green_, mixer_, lo_wav, hi_wav):
+def play_continuosly(switch_, red_, green_, mixer_, lo_wav, hi_wav):
 
     wav = lo_wav
     button_was_pressed = False
@@ -100,9 +102,10 @@ switch = digitalio.DigitalInOut(GPIO_PIN_PUSHBUTTON)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
-# the "internal" LED, for fun
-neopixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
-neopixel.brightness = 0.3
+# # the "internal" LED, for fun
+# neopixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+# neopixel.brightness = 0.3
+neopixel = None
 
 # the new LEDs
 red_led = digitalio.DigitalInOut(PIN_GPIO_RED_LED)
@@ -121,8 +124,8 @@ green_led.direction = digitalio.Direction.OUTPUT
 
 mixer = create_mixer()
 
-lo_wav = load_wavs(["audio/long_lo.wav"])[0]
-hi_wav = load_wavs(["audio/long_hi.wav"])[0]
+lo_wav = load_wavs(["audio/long_lo-b.wav"])[0]
+hi_wav = load_wavs(["audio/long_hi-b.wav"])[0]
 
-play_continuosly(switch, neopixel, red_led, green_led, mixer, lo_wav, hi_wav)
+play_continuosly(switch, red_led, green_led, mixer, lo_wav, hi_wav)
 
